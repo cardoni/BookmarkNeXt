47a7ccb5bd62f78c9a3893da631eb9a3469540b4
Greg Cardoni <greg@cardoni.net>
2014-03-15 20:39:19 -0700

    password_field fix; validations added for: email, pass length, email uniqueness, username min length(2)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,21 @@
 class User < ActiveRecord::Base
 
   has_secure_password
+
   validates :username,
              presence: true,
-             uniqueness: true
+             uniqueness: true,
+             length: { minimum: 2 }
+
+  validates :email,
+             presence: true,
+             uniqueness: true,
+             format: { with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/,
+                       message: 'must be a valid email address' }
+
   validates :password,
              presence: true,
-             length: { minimum: 6 },
+             length: { in: 6..200 },
              on: :create
 
 end
